e2a2d73589a7a6a247cf46183e31c7a6470d44c2
s.masoumbeigi <sbeygi712@gmail.com>
2024-07-08 11:40:46 +0330

    implemented localization for three languages

---
+++ lib/di/di_setup.dart
@@ -0,0 +1,12 @@
+import 'package:injectable/injectable.dart';
+import 'package:get_it/get_it.dart';
+import 'di_setup.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  initializerName: r'$initGetIt',
+  preferRelativeImports: true,
+  asExtension: false,
+)
+void configureDependencies() => $initGetIt(getIt);
--- lib/src/features/app/my_app.dart
+++ lib/src/features/app/my_app.dart
@@ -1,10 +1,12 @@
 import 'package:ava_airline/src/routes/app_routes.dart';
+import 'package:ava_airline/src/shared/resources/localization/locale_provider.dart';
 import 'package:ava_airline/src/shared/resources/resources.dart';
 import 'package:ava_airline/src/shared/resources/theme/bloc/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,27 +17,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => ThemeCubit(),
-      child: BlocBuilder<ThemeCubit, ThemeState>(
-        builder: (BuildContext context, ThemeState state) {
-          return MaterialApp.router(
-            title: 'AVA Airline',
-            theme: _lightTheme.materialThemeData,
-            darkTheme: _darkTheme.materialThemeData,
-            themeMode: context.watch<ThemeCubit>().isDark? ThemeMode.dark : ThemeMode.light,
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: AppLocalizations.supportedLocales,
-            locale: const Locale('en'),
-            debugShowCheckedModeBanner: false,
-            routerConfig: Routes.routes,
-          );
-        },
+    return ChangeNotifierProvider(
+      create: (_) => LocaleProvider(),
+      child: BlocProvider(
+        create: (BuildContext context) => ThemeCubit(),
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (BuildContext context, ThemeState state) {
+            final provider = context.watch<LocaleProvider>();
+            return MaterialApp.router(
+              title: 'AVA Airline',
+              theme: _lightTheme.materialThemeData,
+              darkTheme: _darkTheme.materialThemeData,
+              themeMode: context.watch<ThemeCubit>().isDark
+                  ? ThemeMode.dark
+                  : ThemeMode.light,
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: provider.locale,
+              debugShowCheckedModeBanner: false,
+              routerConfig: Routes.routes,
+            );
+          },
+        ),
       ),
     );
   }
