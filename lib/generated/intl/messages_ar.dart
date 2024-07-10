e1ff2a3ef7cf9e03071b71c690816fc1cd3f62cb
as@2025 <a.soleimani@behpardaz.net>
2024-07-10 15:00:36 +0330

    fix conflict in localization

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -50,9 +50,19 @@ class MessageLookup extends MessageLookupByLibrary {
         "ava_with_you": MessageLookupByLibrary.simpleMessage("آفا مع سلامتك"),
         "baggage": MessageLookupByLibrary.simpleMessage("أمتعة"),
         "baggageTracker": MessageLookupByLibrary.simpleMessage("متعقب الأمتعة"),
+        "batteryDescription2": MessageLookupByLibrary.simpleMessage(
+            "يتم نقل الكراسي المتحركة التي تحتوي على بطارية ليثيوم أيون بسعة قصوى تبلغ 300 واط/ساعة أو بطاريتين بسعة قصوى تبلغ 160 واط/ساعة، بعد التعبئة وموافقة ممثل خطوط ماهان الجوية، في مقصورة الركاب فقط."),
+        "batteryWheelchairDescription": MessageLookupByLibrary.simpleMessage(
+            "يتم نقل الكراسي المتحركة أو غيرها من وسائل المساعدة على الحركة التي تحتوي على بطارية رطبة أو جافة في مخزن البضائع بالطائرة مع حماية البطارية من الدوائر القصيرة، وتغليف آمن، وموافقة ممثل خطوط ماهان الجوية. (إذا كانت البطارية قابلة للإزالة، يتم نقل البطارية المنفصلة في عبوة مقاومة في مخزن الطائرة)."),
+        "batteryWheelchairTitle": MessageLookupByLibrary.simpleMessage(
+            "الكراسي المتحركة التي تعمل بالبطارية"),
         "birthDate": MessageLookupByLibrary.simpleMessage("تاريخ الميلاد"),
         "birth_date": MessageLookupByLibrary.simpleMessage("تاريخ الميلاد"),
         "blind": MessageLookupByLibrary.simpleMessage("كفيف"),
+        "blindDeafDescription": MessageLookupByLibrary.simpleMessage(
+            "لا يحتاج هؤلاء الركاب إلى تقديم شهادة طبية. في حالة السفر الجماعي، يجب تقديم تصريح للتنسيق، ووجود مرافق صحي، قادر، ويتحدث نفس اللغة لهؤلاء الركاب طوال الرحلة. مرافقة هؤلاء الركاب من قبل موظفي خطوط ماهان الجوية ليست خاضعة لرسوم."),
+        "blindDeafTitle":
+            MessageLookupByLibrary.simpleMessage("الركاب المكفوفين والصم"),
         "book": MessageLookupByLibrary.simpleMessage("حجز"),
         "bookFlight": MessageLookupByLibrary.simpleMessage("إحجز رحلة طيران"),
         "bookYourFlight": MessageLookupByLibrary.simpleMessage(
@@ -101,6 +111,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "discountCode": MessageLookupByLibrary.simpleMessage("رمز الخصم"),
         "domestic_destinations":
             MessageLookupByLibrary.simpleMessage("وجهات محلية"),
+        "dubaiToIranFlightCost": MessageLookupByLibrary.simpleMessage(
+            "رحلة من دبي إلى إيران 150 درهم (مع تسجيل SSR)"),
         "during_flight": MessageLookupByLibrary.simpleMessage("خلال الرحلة"),
         "economy": MessageLookupByLibrary.simpleMessage("اقتصادي"),
         "economyClass": MessageLookupByLibrary.simpleMessage("کلاس اکونومی"),
@@ -108,6 +120,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "email": MessageLookupByLibrary.simpleMessage("info@avaair.ir"),
         "enterDiscountCode": MessageLookupByLibrary.simpleMessage(
             "إذا كان لديك رمز خصم، أدخله أدناه واضغط على تطبيق."),
+        "exemptionsDescription": MessageLookupByLibrary.simpleMessage(
+            "يعفى حاملو بطاقات المحاربين القدامى، والمعاقين، وأسر الشهداء المحترمة، وكذلك ركاب رحلات الحج والعتيبات، ومواطني الاتحاد الأوروبي، والأشخاص الذين تزيد أعمارهم عن 65 عامًا من الدفع."),
         "faq": MessageLookupByLibrary.simpleMessage("الأسئلة الشائعة"),
         "female": MessageLookupByLibrary.simpleMessage("أنثى"),
         "firstClass": MessageLookupByLibrary.simpleMessage("فرست کلس"),
@@ -139,17 +153,25 @@ class MessageLookup extends MessageLookupByLibrary {
         "gender_male": MessageLookupByLibrary.simpleMessage("ذكر"),
         "gender_unspecified": MessageLookupByLibrary.simpleMessage("غير محدد"),
         "gold": MessageLookupByLibrary.simpleMessage("ذهبي"),
-        "hintTextPassenger": MessageLookupByLibrary.simpleMessage("1 مسافر"),
+        "hintTextPassenger": MessageLookupByLibrary.simpleMessage("1 راكب"),
         "home": MessageLookupByLibrary.simpleMessage("المنزل"),
         "hotels": MessageLookupByLibrary.simpleMessage("فنادق"),
         "in_flight_entertainment":
             MessageLookupByLibrary.simpleMessage("الترفيه على متن الطائرة"),
         "in_flight_magazine":
             MessageLookupByLibrary.simpleMessage("مجلة الطيران"),
+        "incapacitatedPassengerWheelchairPageSubtitle":
+            MessageLookupByLibrary.simpleMessage(
+                "خدمات خاصة لرحلة خالية من المتاعب"),
+        "incapacitatedPassengerWheelchairPageTitle":
+            MessageLookupByLibrary.simpleMessage(
+                "الركاب من ذوي الاحتياجات الخاصة/ الكرسي المتحرك"),
         "infant": MessageLookupByLibrary.simpleMessage("الرضع"),
         "infantDescription":
             MessageLookupByLibrary.simpleMessage("العمر أقل من 2"),
         "inn": MessageLookupByLibrary.simpleMessage("في"),
+        "internationalFlightCost": MessageLookupByLibrary.simpleMessage(
+            "رحلة دولية من إيران 1,000,000 ريال"),
         "international_destinations":
             MessageLookupByLibrary.simpleMessage("وجهات دولية"),
         "iran": MessageLookupByLibrary.simpleMessage("إيران"),
@@ -174,6 +196,12 @@ class MessageLookup extends MessageLookupByLibrary {
         "lounge_services": MessageLookupByLibrary.simpleMessage("خدمات الصالة"),
         "male": MessageLookupByLibrary.simpleMessage("ذكر"),
         "mashhad": MessageLookupByLibrary.simpleMessage("مشهد"),
+        "mashhadSpecialFaresPageTitle":
+            MessageLookupByLibrary.simpleMessage("رحلة الحج إلى مشهد"),
+        "mashhadSpecialFaresParagraph1": MessageLookupByLibrary.simpleMessage(
+            "مدينة مشهد الواقعة في محافظة خراسان رضوي تعتبر واحدة من المحافظات التاريخية والدينية في إيران. تستضيف هذه المدينة سنويًا حوالي 27 مليون مسافر محلي وأجنبي يسافرون إلى هذا المكان لأداء الحج والمناسبات الدينية. السبب الرئيسي لهذا العدد الكبير من الركاب والسياح هو ضريح الإمام الرضا (ع) في مشهد. هذه المدينة غنية جدًا ثقافيًا أيضًا."),
+        "mashhadSpecialFaresParagraph2": MessageLookupByLibrary.simpleMessage(
+            "قبر العديد من كبار الدين والشعر والأدب مثل الفردوسي الذي كان له تأثير كبير على ثقافة ولغة الإيرانيين يقع في هذه المدينة وضواحيها. كل هذا جعل من مشهد مدينة حديثة في السنوات الأخيرة، حيث توجد الآن مراكز ترفيهية وسياحية وتجارية متنوعة ومتقدمة في كل مكان فيها."),
         "medical_issues": MessageLookupByLibrary.simpleMessage("مشاكل طبية"),
         "message1": MessageLookupByLibrary.simpleMessage(
             "تم تأجيل رحلتك AV123 لمدة 30 دقيقة."),
@@ -203,6 +231,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "no_delay": MessageLookupByLibrary.simpleMessage("بدون تأخير"),
         "none": MessageLookupByLibrary.simpleMessage("لا أحد"),
         "normalRefund": MessageLookupByLibrary.simpleMessage("استرداد عادي"),
+        "noteDescription": MessageLookupByLibrary.simpleMessage(
+            "في طائرات Embraer ERJ-145، يتم استثناء هذه الخدمات من هذه القاعدة. لا يمكن قبول الركاب من ذوي الاحتياجات الخاصة والنساء الحوامل في مقصورة درجة الأعمال في طائرات بوينغ 747. لذلك، يُرجى التأكد من نوع الطائرة قبل الشراء."),
+        "noteTitle": MessageLookupByLibrary.simpleMessage("ملاحظة"),
         "notifications": MessageLookupByLibrary.simpleMessage("إشعارات"),
         "numberOfPassengers":
             MessageLookupByLibrary.simpleMessage("عدد الركاب"),
@@ -331,6 +362,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "title5": MessageLookupByLibrary.simpleMessage("ترويج"),
         "to": MessageLookupByLibrary.simpleMessage("إلى"),
         "toCalender": MessageLookupByLibrary.simpleMessage("إلى"),
+        "toIranFlightCost": MessageLookupByLibrary.simpleMessage(
+            "رحلة دولية إلى إيران 40 دولار"),
         "travelDate": MessageLookupByLibrary.simpleMessage("تاریخ السفر"),
         "travelDates": MessageLookupByLibrary.simpleMessage("تواريخ السفر"),
         "travelServices": MessageLookupByLibrary.simpleMessage("خدمات السفر"),
@@ -339,6 +372,12 @@ class MessageLookup extends MessageLookupByLibrary {
         "unaccompanied_child":
             MessageLookupByLibrary.simpleMessage("طفل بدون مرافق"),
         "version": MessageLookupByLibrary.simpleMessage("الإصدار 1.0"),
+        "wheelchairDescription": MessageLookupByLibrary.simpleMessage(
+            "وفقًا لأنظمة خطوط ماهان الجوية وقدرة كل طائرة، يُسمح فقط للركاب الذين سجلوا طلب الكرسي المتحرك في الكابينة من خلال نظام الحجز وتم إدراج رمز SSR على تذكرتهم بالقبول في المطار واستخدام الكرسي المتحرك. يجب على الركاب الذين يحتاجون إلى كرسي متحرك دون شهادة طبية تسجيل طلبهم عند حجز تذكرتهم. يمكن للركاب استخدام الكرسي المتحرك الشخصي حتى الصعود إلى الطائرة. من الجدير بالذكر أنه إذا كانت السعة ممتلئة، فلن يكون من الممكن تسجيل طلب الكرسي المتحرك."),
+        "wheelchairServiceCostTitle":
+            MessageLookupByLibrary.simpleMessage("تكلفة خدمة الكرسي المتحرك"),
+        "wheelchairTitle":
+            MessageLookupByLibrary.simpleMessage("الكرسي المتحرك"),
         "wheelchair_full":
             MessageLookupByLibrary.simpleMessage("كرسي متحرك في جميع المراحل"),
         "wheelchair_in_map":
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -2290,6 +2290,196 @@ class S {
     );
   }
 
+  /// `Passengers with Special Needs/Wheelchair`
+  String get incapacitatedPassengerWheelchairPageTitle {
+    return Intl.message(
+      'Passengers with Special Needs/Wheelchair',
+      name: 'incapacitatedPassengerWheelchairPageTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Special services for a hassle-free trip`
+  String get incapacitatedPassengerWheelchairPageSubtitle {
+    return Intl.message(
+      'Special services for a hassle-free trip',
+      name: 'incapacitatedPassengerWheelchairPageSubtitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Blind and Deaf Passengers`
+  String get blindDeafTitle {
+    return Intl.message(
+      'Blind and Deaf Passengers',
+      name: 'blindDeafTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `These passengers do not need to provide a medical certificate. In case of group travel, providing a permit for coordination, the presence of a healthy, capable, and same-language companion for these passengers is necessary throughout the journey. Accompanying these passengers by Mahan Airlines personnel is not subject to a fee.`
+  String get blindDeafDescription {
+    return Intl.message(
+      'These passengers do not need to provide a medical certificate. In case of group travel, providing a permit for coordination, the presence of a healthy, capable, and same-language companion for these passengers is necessary throughout the journey. Accompanying these passengers by Mahan Airlines personnel is not subject to a fee.',
+      name: 'blindDeafDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wheelchair`
+  String get wheelchairTitle {
+    return Intl.message(
+      'Wheelchair',
+      name: 'wheelchairTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `According to Mahan Airlines regulations and the capacity of each aircraft, only passengers who have registered their cabin wheelchair request through the reservation system and have the SSR code on their ticket are eligible to be accepted at the airport and use the wheelchair. Passengers who need a wheelchair without a medical certificate must register their request when booking their ticket. Passengers can use their personal wheelchair until boarding the aircraft. It is worth noting that if the capacity is full, registering a wheelchair request is not possible.`
+  String get wheelchairDescription {
+    return Intl.message(
+      'According to Mahan Airlines regulations and the capacity of each aircraft, only passengers who have registered their cabin wheelchair request through the reservation system and have the SSR code on their ticket are eligible to be accepted at the airport and use the wheelchair. Passengers who need a wheelchair without a medical certificate must register their request when booking their ticket. Passengers can use their personal wheelchair until boarding the aircraft. It is worth noting that if the capacity is full, registering a wheelchair request is not possible.',
+      name: 'wheelchairDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Note`
+  String get noteTitle {
+    return Intl.message(
+      'Note',
+      name: 'noteTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `On Embraer ERJ-145 aircraft, these services are exempt from this rule. Accepting passengers with special needs and pregnant women in the business class cabin of Boeing 747 aircraft is not possible. Therefore, please ensure the type of aircraft before purchasing.`
+  String get noteDescription {
+    return Intl.message(
+      'On Embraer ERJ-145 aircraft, these services are exempt from this rule. Accepting passengers with special needs and pregnant women in the business class cabin of Boeing 747 aircraft is not possible. Therefore, please ensure the type of aircraft before purchasing.',
+      name: 'noteDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Battery-Powered Wheelchairs`
+  String get batteryWheelchairTitle {
+    return Intl.message(
+      'Battery-Powered Wheelchairs',
+      name: 'batteryWheelchairTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wheelchairs or other mobility aids with wet or dry batteries are transported in the aircraft's cargo hold with the battery protected against short circuits, safely packaged, and approved by Mahan Airlines representative. (If the battery is removable, the detached battery is carried in a resistant package in the aircraft's hold).`
+  String get batteryWheelchairDescription {
+    return Intl.message(
+      'Wheelchairs or other mobility aids with wet or dry batteries are transported in the aircraft\'s cargo hold with the battery protected against short circuits, safely packaged, and approved by Mahan Airlines representative. (If the battery is removable, the detached battery is carried in a resistant package in the aircraft\'s hold).',
+      name: 'batteryWheelchairDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wheelchairs with lithium-ion batteries with a maximum capacity of 300 W/H or two batteries with a maximum capacity of 160 W/H, after packaging and approval by Mahan Airlines representative, are only carried in the passenger cabin.`
+  String get batteryDescription2 {
+    return Intl.message(
+      'Wheelchairs with lithium-ion batteries with a maximum capacity of 300 W/H or two batteries with a maximum capacity of 160 W/H, after packaging and approval by Mahan Airlines representative, are only carried in the passenger cabin.',
+      name: 'batteryDescription2',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Wheelchair Service Cost`
+  String get wheelchairServiceCostTitle {
+    return Intl.message(
+      'Wheelchair Service Cost',
+      name: 'wheelchairServiceCostTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `International flight from Iran 1,000,000 IRR`
+  String get internationalFlightCost {
+    return Intl.message(
+      'International flight from Iran 1,000,000 IRR',
+      name: 'internationalFlightCost',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `International flight to Iran 40 USD`
+  String get toIranFlightCost {
+    return Intl.message(
+      'International flight to Iran 40 USD',
+      name: 'toIranFlightCost',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Dubai to Iran flight 150 AED (with SSR registration)`
+  String get dubaiToIranFlightCost {
+    return Intl.message(
+      'Dubai to Iran flight 150 AED (with SSR registration)',
+      name: 'dubaiToIranFlightCost',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Holders of veterans, disabled, and martyr families cards, as well as passengers on Hajj and Atabat flights, EU citizens, and individuals over 65 years old are exempt from payment.`
+  String get exemptionsDescription {
+    return Intl.message(
+      'Holders of veterans, disabled, and martyr families cards, as well as passengers on Hajj and Atabat flights, EU citizens, and individuals over 65 years old are exempt from payment.',
+      name: 'exemptionsDescription',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Mashhad Pilgrimage Trip`
+  String get mashhadSpecialFaresPageTitle {
+    return Intl.message(
+      'Mashhad Pilgrimage Trip',
+      name: 'mashhadSpecialFaresPageTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Mashhad city, located in Khorasan Razavi province, is considered one of the historical and religious provinces of Iran. This city annually hosts about 27 million domestic and foreign travelers who travel to this place for pilgrimage and religious ceremonies. The main reason for this volume of passengers and tourists is the shrine of Imam Reza (AS) in Mashhad. This city is also culturally very rich.`
+  String get mashhadSpecialFaresParagraph1 {
+    return Intl.message(
+      'Mashhad city, located in Khorasan Razavi province, is considered one of the historical and religious provinces of Iran. This city annually hosts about 27 million domestic and foreign travelers who travel to this place for pilgrimage and religious ceremonies. The main reason for this volume of passengers and tourists is the shrine of Imam Reza (AS) in Mashhad. This city is also culturally very rich.',
+      name: 'mashhadSpecialFaresParagraph1',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The tomb of many great religious, poetic and literary figures such as Ferdowsi, who has had a significant impact on the culture and language of Iranians, is located in this city and its suburbs. All of these have made Mashhad a modern city in recent years, so much so that diverse and advanced recreational, tourism and commercial centers are now found everywhere in it.`
+  String get mashhadSpecialFaresParagraph2 {
+    return Intl.message(
+      'The tomb of many great religious, poetic and literary figures such as Ferdowsi, who has had a significant impact on the culture and language of Iranians, is located in this city and its suburbs. All of these have made Mashhad a modern city in recent years, so much so that diverse and advanced recreational, tourism and commercial centers are now found everywhere in it.',
+      name: 'mashhadSpecialFaresParagraph2',
+      desc: '',
+      args: [],
+    );
+  }
+
   /// `Pay`
   String get pay {
     return Intl.message(
